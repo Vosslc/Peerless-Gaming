b97e718b8c48ab45a598877cc7fe818c9a3d2f9f
Roundy <nroundy801@gmail.com>
2020-05-06 11:52:22 -0600

    working on an api for tracking stats from ApexL

--- peerless-gaming/server/server.js
+++ peerless-gaming/server/server.js
@@ -5,6 +5,9 @@ const session = require("express-session");
 
 const { SERVER_PORT, CONNECTION_STRING, SECRET } = process.env;
 
+// Controllers 
+const apexCtrl = require("./controllers/apexCtrl")
+
 const app = express();
 
 app.use(express.json());
@@ -17,6 +20,9 @@ app.use(
     cookie: { secure: false }
   })
 );
+
+// Apex Tracker Endpoints
+app.get("public-api.tracker.gg/v2/apex/standard/profile", apexCtrl.getProfileStats);
 
 massive(CONNECTION_STRING).then(database => {
   app.set("db", database);
--- peerless-gaming/src/App.js
+++ peerless-gaming/src/App.js
@@ -1,7 +1,35 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+// import {apex} from 'apexlegendsjs'
+import "./App.css";
+
+console.log("api", process.env.REACT_APP_APEX_KEY);
 
 function App() {
+  // const { REACT_APP_APEX_KEY } = process.env;
+  // const apex = require("apexlegendsjs")(REACT_APP_APEX_KEY);
+
+  // //Returns basic core statistics about the player
+  // apex
+  //   // .getPlayer("DJ vos SLC", "XBOX") //or XBOX or PSN
+  //   .getPlayer("playerName", "PC") //or XBOX or PSN
+  //   .then((response) => {
+  //     console.log(response);
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   });
+
+  // //Returns more detailed information about the player
+  // apex
+  //   // .getDetailedPlayer("DJ vos SLC", "XBOX") //or XBOX or PSN
+  //   .getDetailedPlayer("playerName", "PC") //or XBOX or PSN
+  //   .then((response) => {
+  //     console.log(response);
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   });
+  // console.log(apex.getPlayer("DJ vos SLC", "XBOX"));
   return (
     <div className="App">
       <h1>App.js</h1>
